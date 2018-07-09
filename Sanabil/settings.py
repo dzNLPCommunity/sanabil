140742f427f616755e80b414c271e012cac0e294
Assem Chelli <assem.ch@gmail.com>
2018-07-09 04:46:35 +0100

    default language to fr

--- Sanabil/settings.py
+++ Sanabil/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
