84d49d66d4fc4dad9bff84fd5d4f2c8e3510ec61
ketimaBU <fatimazahrakechida@gmail.com>
2018-10-13 21:56:05 +0100

    more css

--- Sanabil/settings.py
+++ Sanabil/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 
 if DEBUG:
