4ec1ef6adc4da094cfdd9b9ecad2a454b0739ebf
ketimabu <fatimazahrakechida@gmail.com>
2018-11-15 22:37:30 +0100

    Fixing stats queries

--- charity/views.py
+++ charity/views.py
@@ -41,6 +41,7 @@ def age_range(min_age, max_age):
     current = now().date()
     min_date = date(current.year - min_age, current.month, current.day)
     max_date = date(current.year - max_age, current.month, current.day)
+    print(min_age, max_age)
     return Count('date_de_naissance', filter=Q(date_de_naissance__gte=max_date) & Q(date_de_naissance__lte=min_date))
 
 
@@ -49,7 +50,7 @@ class NecessiteuxData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        qs = Necessiteux.objects.all()
+        qs = Necessiteux.objects
         qsFamille = Famille.objects.all()
         gender_qs = qs.annotate(count=Count('sexe'))
         gender_counts = [0, 0]
@@ -68,12 +69,12 @@ class NecessiteuxData(APIView):
         jeunes = age_range(19, 25)
         adults = age_range(26, 59)
         vieux = age_range(60, 150)
-        age_counts = [0, 0, 0, 0, 0]
+        age_counts = [0, 0, 0, 0]
         enfants_count = 0
         if qs.count() > 0:
-            age_qs = qs.annotate(enfants=enfants).annotate(jeunes=jeunes).annotate(adults=adults).annotate(vieux=vieux)
-            age_counts = [age_qs[0].enfants, age_qs[0].jeunes, age_qs[0].adults, age_qs[0].vieux]
-            enfants_count = age_qs[0].enfants
+            age_qs = qs.values('date_de_naissance').annotate(enfants=enfants, jeunes=jeunes, adults=adults, vieux=vieux)
+            age_counts = [age_qs.filter(enfants=1).count(), age_qs.filter(jeunes=1).count(), age_qs.filter(adults=1).count(), age_qs.filter(vieux=1).count()]
+            enfants_count = age_counts[0]
         age_data = {
             "labels": ["Enfants", "Jeunes", "Adultes", "Vieux"],
             "data": age_counts,
