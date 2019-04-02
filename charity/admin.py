2fbb4a858584d62a99ba05d632a3d54b2461ab2e
ketimabu <fatimazahrakechida@gmail.com>
2019-04-02 18:27:40 +0100

    Add missing fields and export

--- charity/admin.py
+++ charity/admin.py
@@ -1,7 +1,9 @@
 from django.apps import apps
 from django.contrib import admin
-
-from charity.models import Association, Famille, Necessiteux, Centre, Besoin, Donneur, AideRecu
+from import_export import resources
+from import_export.admin import ExportActionModelAdmin, ExportMixin
+from import_export.formats import base_formats
+from charity.models import Famille, Necessiteux, Centre, Besoin, Donneur, AideRecu
 
 
 @admin.register(Famille)
@@ -12,14 +14,27 @@ class FamilleAdmin(admin.ModelAdmin):
     list_filter = ["nombre_enfant"]
 
 
+class NecessiteuxResource(resources.ModelResource):
+    class Meta:
+        model = Necessiteux
+
+
 @admin.register(Necessiteux)
-class NecessiteuxAdmin(admin.ModelAdmin):
+class NecessiteuxAdmin(ExportActionModelAdmin):
     list_display = ["nom", "prenom", "tel", "association"]
     list_display_links = ["nom", "prenom"]
     search_fields = ["nom", "prenom", "tel"]
     list_filter = ['sexe', 'niveau_scolaire', 'pointure', 'taille', 'situation_familiale', 'situation_professionelle',
-                   'est_orphelin', 'represent_famille', 'degré_nécessite', 'archivé', 'est_permanent']
+                   'est_orphelin', 'represent_famille', 'degré_nécessite', 'archivé', 'type']
     radio_fields = {'sexe': admin.HORIZONTAL, 'degré_nécessite': admin.HORIZONTAL}
+    resource_class = NecessiteuxResource
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.CSV,
+            base_formats.XLS,
+        )
+        return [f for f in formats if f().can_export()]
 
 
 @admin.register(Centre)
@@ -39,32 +54,32 @@ def satisfy_need(modeladmin, request, queryset):
 satisfy_need.short_description = 'Satisfaire les besoins sélectionnés'
 
 
-class IsPermarentNeedy(admin.SimpleListFilter):
-    title = 'nécessiteux permanent'
-    parameter_name = 'permarent_needy'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('Oui', 'Oui'),
-            ('Non', 'Non'),
-        )
-
-    def queryset(self, request, queryset):
-        value = self.value()
-        if value == 'Oui':
-            return queryset.filter(necessiteux__est_permanent=True)
-        elif value == 'Non':
-            return queryset.exclude(necessiteux__est_permanent=True)
-        return queryset
+class BesoinResource(resources.ModelResource):
+    class Meta:
+        model = Besoin
 
 
 @admin.register(Besoin)
-class BesoinAdmin(admin.ModelAdmin):
+class BesoinAdmin(ExportActionModelAdmin):
     list_display = ["nom", "type", "est_urgent", "association"]
     date_hierarchy = "date_planification"
     search_fields = ["nom", "description"]
-    list_filter = ["type", "est_urgent", "est_satisfait", "association", IsPermarentNeedy]
+    list_filter = ["type", "est_urgent", "est_satisfait", "association", "type"]
     actions = [satisfy_need, ]
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "necessiteux":
+            kwargs["queryset"] = Necessiteux.objects.filter(archivé=False)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+    resource_class = BesoinResource
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.CSV,
+            base_formats.XLS,
+        )
+        return [f for f in formats if f().can_export()]
 
 
 @admin.register(Donneur)
@@ -80,6 +95,3 @@ class AideRecuAdmin(admin.ModelAdmin):
     list_display = ["__str__", "donneur", "association"]
     date_hierarchy = "date_reception"
     list_filter = ["type", "association"]
-
-
-
--- charity/models.py
+++ charity/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from datetime import date
 from base.models import Commune, NiveauScolaire, SituationFamiliale, SituationProfessionelle, CentreType, \
     DonType, DonneurType
@@ -28,8 +28,20 @@ class Necessiteux(models.Model):
     association = models.ForeignKey(Association, null=True, on_delete=models.SET_NULL, related_name="members")
     nom = models.CharField(max_length=500)
     prenom = models.CharField('prénom', max_length=100)
+    jeune_fille = models.CharField('Nom de jeune fille', max_length=100, blank=True, null=True)
+    part_de = models.CharField('De la part de', max_length=100, blank=True, null=True)
+    nin = models.CharField('Numéro pièce d’identité', max_length=9, blank=True, null=True,
+        validators=[
+            RegexValidator(
+                regex='\d{9}$',
+                message='Champ invalide',
+            ),
+        ]
+    )
     date_de_naissance = models.DateField(blank=True, null=True)
     sexe = models.CharField(max_length=1, choices=[("F", "Female"), ("M", "Male")])
+    type = models.SmallIntegerField(default=0,
+                                    choices=[(0, 'Permanent'), (1, 'Temporaire'), (2, 'Voyageur en détresse')])
     niveau_scolaire = models.ForeignKey(NiveauScolaire, on_delete=models.SET_NULL, blank=True, null=True)
     tel = models.CharField('tél', max_length=20, blank=True, null=True)
     pointure = models.IntegerField(blank=True, null=True, default=36,
@@ -47,7 +59,6 @@ class Necessiteux(models.Model):
     represent_famille = models.BooleanField(default=False)
     appartient_centre = models.ForeignKey(Centre, on_delete=models.SET_NULL, blank=True, null=True)
     archivé = models.BooleanField(default=False)
-    est_permanent = models.BooleanField(default=False)
 
     def get_need(self):
         needs_qs = Besoin.objects.filter(necessiteux=self.id).values_list('nom', flat=True)
@@ -71,17 +82,19 @@ class Necessiteux(models.Model):
 
 class Besoin(models.Model):
     association = models.ForeignKey(Association, on_delete=models.CASCADE)
-    necessiteux = models.ForeignKey(Necessiteux, on_delete=models.CASCADE, null=True, blank=True)
+    necessiteux = models.ManyToManyField(Necessiteux, null=True, blank=True)
     centre = models.ForeignKey(Centre, on_delete=models.CASCADE, null=True, blank=True)
     nom = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     type = models.ForeignKey(DonType, on_delete=models.CASCADE)
     est_urgent = models.BooleanField(default=False)
     date_limite = models.DateField(blank=True, null=True)
+    expire_le = models.DateField(blank=True, null=True)
     montant = models.IntegerField(default=0, null=True, blank=True)
     date_planification = models.DateField(blank=True, null=True)
     date_remise_don = models.DateField(blank=True, null=True)
     est_satisfait = models.BooleanField(default=False)
+    délivré_par = models.TextField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return "{}. {}".format(self.id, self.nom)
